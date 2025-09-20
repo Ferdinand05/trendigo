70c7898b9267bd09f30f734ce7c2b2f154cd4668
Ferdinand05 <fkoryanto@gmail.com>
2025-09-20 11:25:51 +0700

    change : make controller special for midtrans notification

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\HandleNotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // midtrans notif callback / auto
-Route::post('/midtrans/notification', [CheckoutController::class, 'notification']);
+Route::post('/midtrans/notification', [HandleNotificationController::class, 'notification']);
